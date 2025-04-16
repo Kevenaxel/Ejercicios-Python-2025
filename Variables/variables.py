b01e5499b8c663e712b9fe02d5b0b28076818c68
Kevenaxel <hectorsastre476@gmail.com>
2025-04-16 10:01:49 -0600

    new

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,5 +1,40 @@
-a = 15
-b = 15
-c = a + b
+#a+b se hace la suma que es un 30
 
-print(a)
+#a = 15
+#b = 15
+#c = a + b
+
+#print(a+b)
+
+#Muestra el ultimo nombre como fuel el ultimo en escribitr
+
+#nombre = "Kevin"
+#nombre = "Anthony"
+#nombre = "Mauricio"
+
+#print(nombre)
+
+
+#Normal Muestra El codigo Hola Kevin que tal te fue hoy
+#nombre = "Kevin"
+#bienvenido = "Kevin " + nombre + " Que tal te fue el dia de hoy"
+#print(bienvenido)
+
+#Muestra con un numero contactenado
+
+#nombre = 5
+#bienvenido = f"hola {nombre} Que tal te fue el dia de hoy"
+
+#print(bienvenido)
+
+#Buscamos una letra con un codigo para mostrar si exitse o no
+#nombre = 5
+#bienvenido = f"hola {nombre} Que tal te fue el dia de hoy"
+#print("ola" in bienvenido)
+
+
+#Buscamos u  texto que no exitse
+nombre = 5
+bienvenido = f"hola {nombre} Que tal te fue el dia de hoy"
+
+print("Hola" not in bienvenido)
